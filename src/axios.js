dc368101fdd0e737baa6079568dafc351238778b
Reymel <anianoreymel1995@gmail.com>
2022-09-15 16:29:38 +0800

    net

---
+++ src/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+/** base url to make request to the movie database
+ */
+
+const instance = axios.create({
+  baseUrl:"https://api.themoviedb.org/3",
+});
+
+export default instance;
---
+++ src/requests.js
@@ -0,0 +1,5 @@
+ const API_KEY = "a4e9389d16e78a9bd03c8ba0a4dc50ca";
+
+ const requests = {
+  
+ }
